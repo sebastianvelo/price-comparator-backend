cd520a942834635ce27b24cc383edf8dd2e210dc
Sebastian <sebastian.velo@mercadolibre.com>
2022-05-28 08:16:20 -0300

    update package json

--- src/app.init.ts
+++ src/app.init.ts
@@ -4,12 +4,6 @@ import cors from "cors";
 import helmet from "helmet";
 
 const init = (app: Express) => {
-    app.use(function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "http://http://192.168.0.100:5000/"); // update to match the domain you will make the request from
-        res.header("Access-Control-Allow-Origin", "https://comparador-precios.herokuapp.com/"); // update to match the domain you will make the request from
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    });
     app.use(cors());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
